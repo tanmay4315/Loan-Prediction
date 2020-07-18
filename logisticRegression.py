b940802b822399b66d00b003c13bf310f876d218
Tanmay Kumar <44299800+tanmay4315@users.noreply.github.com>
2020-07-18 13:52:01 +0530

    Update logisticRegression.py

--- logisticRegression.py
+++ logisticRegression.py
@@ -93,7 +93,7 @@ beta=[0]*(ncols+1)
 for i in range(1):
     beta[-1]=beta[-1]+0.0000001*sum([(train_y[j]-(1/(1+math.exp(-(sum([train_x[j][l]*beta[l] for l in range(len(train_x[1]))]))))))*1 for j in range(len(train_x))])
     for k in range(len(train_x[1])): #here k used for updating value of beta
-        beta[k]=beta[k]+0.00000001*sum([(train_y[j]-(1/(1+math.exp(-(sum([train_x[j][l]*beta[l] for l in range(len(train_x[1]))]))))))*train_x[j][k] +beta[-1] for j in range(len(train_x))])
+        beta[k]=beta[k]+0.00000001*sum([(train_y[j]-(1/(1+math.exp(-(sum([train_x[j][l]*beta[l] for l in range(len(train_x[1]))])+beta[-1])))))*train_x[j][k]  for j in range(len(train_x))])
 
 #testing Model
 correct=0
